c182107fb846cae3f3990e7e34b647e8093b7758
pdcxs <pdcxs@outlook.com>
2025-10-13 11:09:33 +0800

    update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -24,16 +24,15 @@ return {
       },
     },
   },
-  build = (package.config:sub(1, 1) == "\\") -- is windows
-      and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource true"
-    or "build",
+  build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    or "make",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-mini/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
