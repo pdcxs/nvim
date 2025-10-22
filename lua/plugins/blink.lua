feec78ecf4fec96c59aad9a729a9f12f39e00cca
pdcxs <pdcxs@outlook.com>
2025-10-22 13:12:21 +0800

    combine blink and avante

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,19 @@
 return {
   "Saghen/blink.cmp",
+  dependencies = {
+    "Kaiser-Yang/blink-cmp-avante",
+  },
   opts = {
+    sources = {
+      default = { "avante", "lsp", "path", "buffer" },
+      providers = {
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
+          opts = {},
+        },
+      },
+    },
     keymap = {
       preset = "enter",
       ["<Tab>"] = { "select_next", "fallback" },
