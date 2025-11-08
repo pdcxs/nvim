4b452b960d8222b33643e26a172448c37cff123c
pdcxs <pdcxs@outlook.com>
2025-11-08 10:12:20 +0800

    Ignore directory buffer when restore projects.

--- lua/plugins/extend-bufferline.lua
+++ lua/plugins/extend-bufferline.lua
@@ -30,4 +30,19 @@ return {
       desc = "Previous buffer",
     },
   },
+  opts = {
+    options = {
+      custom_filter = function(buf, _)
+        -- Directory buffers appears after restoring the session and
+        -- they should be ignored.
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+        local state = vim.uv.fs_stat(buf_name)
+        if state and state.type == "directory" then
+          return false
+        end
+
+        return true
+      end,
+    },
+  },
 }
